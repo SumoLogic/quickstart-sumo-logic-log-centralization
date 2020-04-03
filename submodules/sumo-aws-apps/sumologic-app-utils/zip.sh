c9635ea8d5b5a852591080b32e39487cc6aabfcd
sourabh <sourabh@sumologic.com>
2020-04-03 11:37:03 +0530

    Getting latest ZIP file from GitHub.

--- submodules/sumo-aws-apps/sumologic-app-utils/zip.sh
+++ submodules/sumo-aws-apps/sumologic-app-utils/zip.sh
@@ -1,8 +1,8 @@
-if [ -f sumo_app_utils.zip ]; then
+if [[ -f sumo_app_utils.zip ]]; then
     rm sumo_app_utils.zip
 fi
 
-if [ ! -f sumo_app_utils.zip ]; then
+if [[ ! -f sumo_app_utils.zip ]]; then
     echo "copying zip file from sumoloigc-aws-lambda repo."
     curl -LJO https://github.com/SumoLogic/sumologic-aws-lambda/raw/sourabh-aws-observability/sumologic-aws-observability/SumoLogicAWSObservabilityHelper/SumoLogicAWSObservabilityHelper.zip
     mv SumoLogicAWSObservabilityHelper.zip sumo_app_utils.zip
